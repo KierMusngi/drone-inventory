f06e262d517803b5581054fcdea75c47fa36d3e0
Kier Musngi <musngi.kier@gmail.com>
2020-01-06 01:51:58 +0800

    WIP: Default layout

---
+++ drone-app/layouts/default.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      floating
+      mini-variant
+      temporary
+      app
+      overflow
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-qrcode-scan</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-package-variant-closed</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      clipped-left
+      app
+    >
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+      />
+      <v-toolbar-title>Drone App</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <nuxt />
+    </v-content>
+
+    <v-footer
+      app
+    >
+      <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+        drawer: false,
+    }),
+  }
+</script>
+
